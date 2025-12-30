10f443ed0c806fd5982ab477b8ab4dd865186c76
Marko Nedzbala <marko.nedzbala@gmail.com>
2025-12-30 17:15:35 -0500

    EOD and EOY

--- Udemy_Course/DiaryApp/Program.cs
+++ Udemy_Course/DiaryApp/Program.cs
@@ -1,7 +1,13 @@
+using DiaryApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
@@ -22,7 +28,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Home}/{action=MyPage}/{id?}")
     .WithStaticAssets();
 
 
